c3f349da2469f342d134fbfa5d53a872cccbb6de
Svitlana Suslenkova <svitlana.suslenkova@gmail.com>
2024-09-26 16:03:11 +0200

    header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,22 +1,26 @@
 import Image from 'next/image';
 import Avatar from '../images/image-avatar.jpg';
 import Moon from '../images/icon-moon.svg';
+//import Sun from '../images/icon-sun.svg'
 export default function Header() {
   return (
-    <header className={`flex h-[4.5rem] md:h-[5rem] bg-header`}>
+    <header
+      className={`flex h-[4.5rem] md:h-[5rem] bg-header xl:flex-col xl:w-[6.44rem] xl:h-screen xl:rounded-r-[1.25rem]`}
+    >
       <section
         className={`w-[4.5rem] aspect-square bg-primary rounded-r-[1.25rem] overflow-hidden md:w-[5rem]   relative before:block 
             before:absolute before:w-full before:h-full before:bg-[url('../images/logo.svg')] before:bg-no-repeat
-            before:bg-center before:bg-[length:38.8%] `}
+            before:bg-center before:bg-[length:38.8%] xl:w-full`}
       >
         <div
-          className={`mt-[2.25rem] w-[4.5rem] aspect-[2/1] bg-[#9277FF] rounded-tl-[1.25rem] md:w-[5rem] md:mt-[2.5rem]`}
+          className={`mt-[2.25rem] w-[4.5rem] aspect-[2/1] bg-[#9277FF] rounded-tl-[1.25rem] md:w-[5rem] md:mt-[2.5rem] xl:w-full 
+            xl:mt-[3.22rem] `}
         ></div>
       </section>
       <section
-        className={`grid place-content-center w-[4.5rem] md:w-[5rem] aspect-square ml-auto`}
+        className={`grid place-content-center w-[4.5rem] md:w-[5rem] aspect-square ml-auto xl:mt-auto xl:w-full xl:ml-0`}
       >
-        <button className={`p-2`}>
+        <button className={`p-2 hover:brightness-150`}>
           <Image
             className={`rounded-full`}
             src={Moon}
@@ -27,10 +31,11 @@ export default function Header() {
         </button>
       </section>
       <section
-        className={`w-[4.5rem] md:w-[5rem] aspect-square grid place-items-center border-l border-l-[#494E6E]`}
+        className={`w-[4.5rem] md:w-[5rem] aspect-square grid place-items-center border-l border-l-[#494E6E] xl:w-full 
+            xl:ml-0 xl:border-l-0 xl:border-t xl:border-t-[#494E6E]`}
       >
         <Image
-          className={`rounded-full`}
+          className={`rounded-full xl:w-10`}
           src={Avatar}
           width={32}
           height={32}
