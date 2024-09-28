771a904c5efc50a3f887d24ba67c38788a7b38d0
Svitlana Suslenkova <svitlana.suslenkova@gmail.com>
2024-09-28 23:25:20 +0200

    style

--- src/components/InvoiceCard.tsx
+++ src/components/InvoiceCard.tsx
@@ -1,11 +1,13 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import ArrowRight from '../images/icon-arrow-right.svg';
 import Status from './Status';
 //import ArrowDown from '../images/icon-arrow-down.svg'
 
 export default function InvoiceCard() {
   return (
-    <button
+    <Link
+      href="/viewinvoice"
       className={`grid grid-cols-2 sm:grid-cols-[auto,auto,auto,auto,auto] md:grid-cols-[2fr,3fr,4fr,3fr,2fr,1fr] grid-rows-3 sm:grid-rows-1 items-center sm:justify-items-center
          bg-card p-6 px-[7.34%] sm:px-0 sm:py-4 sm:h-h18  rounded-lg w-full shadow-smsh
          focus:border focus:border-primary md:pl-6`}
@@ -37,6 +39,6 @@ export default function InvoiceCard() {
       <div className={`hidden sm:w-full sm:h-full md:grid place-items-center`}>
         <Image src={ArrowRight} alt=">" width={7} height={14} />
       </div>
-    </button>
+    </Link>
   );
 }
