7dcbf9835e0209739acfb4cb70ca6693ca87f4fa
Svitlana Suslenkova <svitlana.suslenkova@gmail.com>
2024-09-28 10:20:27 +0200

    style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,9 @@ export default function Home() {
     <main className={`grid xl:grid-cols-[6.44rem,auto]`}>
       <Header />
       <div className={`grid justify-items-center w-full content-start`}>
-        <div className={`grid content-center w-full max-w-[45.63rem] `}>
+        <div
+          className={`grid content-center w-full max-w-[45.63rem] 2xl:max-w-[60rem] `}
+        >
           <InvoiceCards />
         </div>
       </div>
