19b4e88a77691be67859eb22f79d3dfe12927179
Svitlana Suslenkova <svitlana.suslenkova@gmail.com>
2024-09-29 16:55:04 +0200

    style

--- src/components/InvoiceCard.tsx
+++ src/components/InvoiceCard.tsx
@@ -16,7 +16,7 @@ export default function InvoiceCard({ invoice }) {
       href="/viewinvoice"
       className={`grid grid-cols-2 sm:grid-cols-[auto,auto,auto,auto,auto] md:grid-cols-[2fr,3fr,4fr,3fr,2fr,1fr] grid-rows-3 sm:grid-rows-1 items-center sm:justify-items-center
          bg-card p-6 px-[7.34%] sm:px-0 sm:py-4 sm:h-h18  rounded-lg w-full shadow-smsh
-         focus:border focus:border-primary md:pl-6`}
+         focus:border focus:border-primary hover:border hover:border-primary md:pl-6`}
     >
       <p
         className={`justify-self-start sm:justify-self-center mb-3.5 sm:mb-0 text-sm15 leading-sm15 font-bold tracking-em0016`}
