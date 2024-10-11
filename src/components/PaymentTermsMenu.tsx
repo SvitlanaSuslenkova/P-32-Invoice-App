80fced9664e6db8f71536baa927d80e48552b875
Svitlana Suslenkova <svitlana.suslenkova@gmail.com>
2024-10-11 08:55:10 +0200

    style

--- src/components/PaymentTermsMenu.tsx
+++ src/components/PaymentTermsMenu.tsx
@@ -1,52 +1,34 @@
-//import { useForm, FormProvider, useFormContext } from 'react-hook-form';
+//import { useFormContext } from 'react-hook-form';
+
 export default function PaymentTermsMenu({
   setPaymentTerms,
   setIsPaymentTermsMenu,
 }) {
-  // const { setValue } = useFormContext();
-  function handleonClick(e) {
+  //const { setValue } = useFormContext();
+  function handleonClick(e, termValue: number) {
     e.preventDefault();
-    const value = e.target.value;
-    console.log(value);
-    setPaymentTerms(value);
-
-    // setValue(`paymentTerms`, value.toString());
-
+    console.log(termValue);
+    setPaymentTerms(termValue);
+    //setValue(`paymentTerms`, termValue.toString());
     setIsPaymentTermsMenu(false);
   }
-
+  const paymentTermValues = [1, 7, 14, 30];
   return (
     <div
       className={`absolute w-60 mt-2 black15 z-12 grid grid-cols-1 shadow-bsh rounded-lg bg-card`}
     >
-      <button
-        className={`capitalize grid place-items-start items-center px-6 py-4 h-12 border-b border-muted-darker hover:text-primary`}
-        value={1}
-        onClick={(e) => handleonClick(e)}
-      >
-        Net 1 day
-      </button>
-      <button
-        className={`capitalize grid place-items-start items-center px-6 py-4 h-12 border-b border-muted-darker hover:text-primary`}
-        value={7}
-        onClick={(e) => handleonClick(e)}
-      >
-        Net 7 days
-      </button>
-      <button
-        className={`capitalize grid place-items-start items-center px-6 py-4 h-12 border-b border-muted-darker hover:text-primary`}
-        value={14}
-        onClick={(e) => handleonClick(e)}
-      >
-        Net 14 days
-      </button>
-      <button
-        className={`capitalize grid place-items-start items-center px-6 py-4 h-12 border-b border-muted-darker hover:text-primary`}
-        value={30}
-        onClick={(e) => handleonClick(e)}
-      >
-        Net 30 days
-      </button>
+      {paymentTermValues &&
+        paymentTermValues.map((termValue) => (
+          <button
+            key={termValue}
+            className={`capitalize text-left px-6 py-4 h-12 border-b border-muted-darker hover:text-primary`}
+            onClick={(e) => handleonClick(e, termValue)}
+          >
+            <span>Net </span>
+            {termValue}
+            {termValue == 1 ? <span> day</span> : <span> days</span>}
+          </button>
+        ))}
     </div>
   );
 }
